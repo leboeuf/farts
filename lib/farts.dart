1e8817af9522bd3a7edd3fb4d37ccaa1d7a4244e
leboeuf <ml@maximeleboeuf.com>
2024-07-11 22:14:59 -0400

    Create ChartData and draw debug text

--- lib/farts.dart
+++ lib/farts.dart
@@ -15,24 +15,24 @@ class Fart extends StatelessWidget {
   final ChartStyle _chartStyle;
 
   /// The series to draw on the chart, including indicators.
-  final List<Series>? _series;
-  // Series {
-  //    SeriesType type, // chart or indicator, can have many of each type
-  //    List<> data
-  // }
+  final ChartData _chartData;
 
   /// Creates a Flutter chart.
-  Fart(this._size, {super.key, ChartStyle? chartStyle, series})
+  Fart(this._size, {super.key, ChartStyle? chartStyle, ChartData? chartData})
       : _chartStyle = chartStyle ?? ChartStyle(),
-        _series = series;
+        _chartData = chartData ?? ChartData();
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
       size: _size,
-      painter: ChartPainter(_chartStyle, _series),
+      painter: ChartPainter(_chartStyle, _chartData),
     );
   }
+}
+
+class ChartData {
+  late List<Series> series;
 }
 
 class Series {
--- lib/painters/chart_painter.dart
+++ lib/painters/chart_painter.dart
@@ -8,8 +8,11 @@ class ChartPainter extends CustomPainter {
   /// The [ChartStyle] to use to draw the chart.
   final ChartStyle _chartStyle;
 
+  /// Data to draw on the chart. Includes tick data, indicators and annotations.
+  final ChartData _chartData;
+
   /// Creates a [ChartPainter] that draws a chart using the given [ChartStyle].
-  ChartPainter(this._chartStyle, List<Series>? series);
+  ChartPainter(this._chartStyle, this._chartData);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -33,6 +36,37 @@ class ChartPainter extends CustomPainter {
         Paint()
           ..color = _chartStyle.colors.lineColor
           ..strokeWidth = 4);
+
+    // Draw debug text
+    _drawDebugText(size, canvas);
+  }
+
+  void _drawDebugText(Size size, Canvas canvas) {
+    const textStyle = TextStyle(
+      color: Colors.red,
+      fontSize: 30,
+    );
+
+    final textSpan = TextSpan(
+      text:
+          '${size.width.toStringAsFixed(0)} x ${size.height.toStringAsFixed(0)}',
+      style: textStyle,
+    );
+
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+
+    final xCenter = (size.width - textPainter.width) / 2;
+    final yCenter = (size.height - textPainter.height) / 2;
+    final offset = Offset(xCenter, yCenter);
+    textPainter.paint(canvas, offset);
   }
 
   @override
