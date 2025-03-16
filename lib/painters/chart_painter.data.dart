ee1145b7a225b303501a3782720105b5fb4ecb18
leboeuf <ml@maximeleboeuf.com>
2025-03-15 20:22:07 -0400

    Move drawing methods to right partial class

--- lib/painters/chart_painter.data.dart
+++ lib/painters/chart_painter.data.dart
@@ -41,4 +41,29 @@ extension Data on ChartPainter {
             ..strokeWidth = 2);
     }
   }
+
+  /// Calculate the width available to draw the ticks after
+  /// removing the legend, padding, etc.
+  double _calculateAvailableWidthForData(Rect chartArea) {
+    return chartArea.width -
+        _chartStyle.rightLegendWidth -
+        _chartStyle.chartPadding.horizontal -
+        _chartStyle.spacingBeforeYAxis;
+  }
+
+  /// Translates a [price] into a vertical screen coordinate.
+  /// [yMin] is the top of the drawing area and [yMax] is the bottom.
+  int _worldToScreen(
+    TickCollection tickData,
+    double price,
+    int yMin,
+    int yMax,
+  ) {
+    final range = tickData.max - tickData.min;
+
+    final yProp = 1 - ((price - tickData.min) / range);
+    final yOffset = yProp * (yMax - yMin);
+
+    return yMin + yOffset.toInt();
+  }
 }
--- lib/painters/chart_painter.debug.dart
+++ lib/painters/chart_painter.debug.dart
@@ -28,29 +28,4 @@ extension Debug on ChartPainter {
     final offset = Offset(xCenter, yCenter);
     textPainter.paint(canvas, offset);
   }
-
-  /// Calculate the width available to draw the ticks after
-  /// removing the legend, padding, etc.
-  double _calculateAvailableWidthForData(Rect chartArea) {
-    return chartArea.width -
-        _chartStyle.rightLegendWidth -
-        _chartStyle.chartPadding.horizontal -
-        _chartStyle.spacingBeforeYAxis;
-  }
-
-  /// Translates a [price] into a vertical screen coordinate.
-  /// [yMin] is the top of the drawing area and [yMax] is the bottom.
-  int _worldToScreen(
-    TickCollection tickData,
-    double price,
-    int yMin,
-    int yMax,
-  ) {
-    final range = tickData.max - tickData.min;
-
-    final yProp = 1 - ((price - tickData.min) / range);
-    final yOffset = yProp * (yMax - yMin);
-
-    return yMin + yOffset.toInt();
-  }
 }
